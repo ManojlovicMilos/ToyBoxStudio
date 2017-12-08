b4ddacb64cb2eadf3b69b20dd83985c1731a16af
Manojlović Miloš <manojlovicsmilos@hotmail.com>
2017-12-08 11:21:07 +0100

    Project Tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { ElectronService } from 'ngx-electron';
 
 import { Project } from "./project/project.model";
@@ -15,7 +15,7 @@ export class AppComponent
   private _Title:string;
   private _Current:Project;
   private _SideBarOption:number;
-  public constructor(private _ElectronService: ElectronService, private Reference: ChangeDetectorRef)
+  public constructor(private _ElectronService: ElectronService, private _Zone:NgZone)
   {
     this._Title = 'ToyBox Studio';
     this._SideBarOption = 0;
@@ -30,12 +30,13 @@ export class AppComponent
   }
   private ProjectLoaded(Event, Data)
   {
-      this._Current.Load(Data);
-      //this.Reference.detach();
-      //this.Reference.detectChanges();
+      this._Zone.run(function() {
+        this._Current.Load(Data);
+      }.bind(this));
   }
   private SelectOption(Option:number) : void
   {
+    console.log(Option);
     if(Option == this._SideBarOption) this._SideBarOption = -1;
     else this._SideBarOption = Option;
   }
